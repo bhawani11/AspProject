8f1992644b760394bf95c1e555b0c26a1f00dcf3
bhawani11 <62748560+bhawani11@users.noreply.github.com>
2022-01-27 09:51:50 +0530

    Add files via upload
    Added driver and driver path in file

--- AspireSeleniumTestng/src/Aspirepac/InventoryTest.java
+++ AspireSeleniumTestng/src/Aspirepac/InventoryTest.java
@@ -15,77 +15,75 @@ import org.testng.annotations.AfterMethod;
 
 public class InventoryTest {
 	public WebDriver driver;
-  
-	NewTest newTest=new NewTest();
-	
-  @BeforeMethod
-  public void setUp() throws Exception {
-	  System.setProperty("webdriver.chrome.driver", "E:\\SAspire\\chromedriver.exe");
-      driver=new ChromeDriver();
-      
-      driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
-      driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-      driver.manage().window().maximize();
-      driver.manage().deleteAllCookies();
-      driver.get("https://aspireapp.odoo.com/web/login");
-      login("user@aspireapp.com","@sp1r3app");
-      
-  }
-   
-  @Test(priority=1)
-  public void clickAndVerifyInventoryPage() {
-    driver.findElement(By.xpath("//div[@class=\"o_apps\"]/child::*[2]")).click();
-    boolean flag=driver.findElement(By.xpath("//span[text()='Receipts']")).isDisplayed();
-    Assert.assertTrue(flag);
-  }
-  
-  @Test(priority=2)
-  public void verifyProduct() {
-	  driver.findElement(By.xpath("//div[@class=\"o_apps\"]/child::*[2]")).click();
-	  driver.findElement(By.xpath("//span[text()='Receipts']")).isDisplayed();
-	  driver.findElement(By.partialLinkText("Products")).click();
-	  driver.findElement(By.xpath("//a//span[text()='Products']")).isDisplayed();
-	  driver.findElement(By.xpath("//a//span[text()='Products']")).click();
-	  driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).isDisplayed();
-      driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).click();
-      driver.findElement(By.xpath("//input[@placeholder=\"Product Name\"]")).sendKeys("GoldLoan");
-      }
-  
 
-  @Test(priority=3)
-  public void verifyUpdateValue() {
-	  driver.findElement(By.xpath("//div[@class=\"o_apps\"]/child::*[2]")).click();
-	  driver.findElement(By.xpath("//span[text()='Receipts']")).isDisplayed();
-	  driver.findElement(By.partialLinkText("Products")).click();
-	  driver.findElement(By.xpath("//a//span[text()='Products']")).isDisplayed();
-	  driver.findElement(By.xpath("//a//span[text()='Products']")).click();
-	  driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).isDisplayed();
-      driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).click();
-      driver.findElement(By.xpath("//input[@placeholder=\"Product Name\"]")).sendKeys("GoldLoan");
-      driver.findElement(By.cssSelector("button.btn.btn-primary.o_form_button_save")).click();
-	  boolean flag1=driver.findElement(By.xpath("//*[contains(text(),'Update Quantity')]")).isDisplayed();
-      Assert.assertTrue(flag1);
-      driver.findElement(By.xpath("//*[contains(text(),'Update Quantity')]")).click();
-      driver.findElement(By.cssSelector("button.btn.btn-primary.o_list_button_add")).click();
-      
-//      webElement e=driver.findElement(By.xpath("(//input[@class=\"o_input ui-autocomplete-input\"])[1]")).click();
-//            Select s=new Select(e)
-  }
-  
-  public void login(String x, String y) throws Exception
-	{
+	NewTest newTest = new NewTest();
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://aspireapp.odoo.com/web/login");
+		login("user@aspireapp.com", "@sp1r3app");
+
+	}
+
+	@Test(priority = 1)
+	public void clickAndVerifyInventoryPage() {
+		driver.findElement(By.xpath("//div[@class=\"o_apps\"]/child::*[2]")).click();
+		boolean flag = driver.findElement(By.xpath("//span[text()='Receipts']")).isDisplayed();
+		Assert.assertTrue(flag);
+	}
+
+	@Test(priority = 2)
+	public void verifyProduct() {
+		driver.findElement(By.xpath("//div[@class=\"o_apps\"]/child::*[2]")).click();
+		driver.findElement(By.xpath("//span[text()='Receipts']")).isDisplayed();
+		driver.findElement(By.partialLinkText("Products")).click();
+		driver.findElement(By.xpath("//a//span[text()='Products']")).isDisplayed();
+		driver.findElement(By.xpath("//a//span[text()='Products']")).click();
+		driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).isDisplayed();
+		driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).click();
+		driver.findElement(By.xpath("//input[@placeholder=\"Product Name\"]")).sendKeys("GoldLoan");
+	}
+
+	/**
+	* 
+	*/
+	@Test(priority = 3)
+	public void verifyUpdateValue() {
+		driver.findElement(By.xpath("//div[@class=\"o_apps\"]/child::*[2]")).click();
+		driver.findElement(By.xpath("//span[text()='Receipts']")).isDisplayed();
+		driver.findElement(By.partialLinkText("Products")).click();
+		driver.findElement(By.xpath("//a//span[text()='Products']")).isDisplayed();
+		driver.findElement(By.xpath("//a//span[text()='Products']")).click();
+		driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).isDisplayed();
+		driver.findElement(By.cssSelector("button.btn.btn-primary.o-kanban-button-new")).click();
+		driver.findElement(By.xpath("//input[@placeholder=\"Product Name\"]")).sendKeys("GoldLoan");
+		driver.findElement(By.cssSelector("button.btn.btn-primary.o_form_button_save")).click();
+		boolean flag1 = driver.findElement(By.xpath("//*[contains(text(),'Update Quantity')]")).isDisplayed();
+		Assert.assertTrue(flag1);
+		driver.findElement(By.xpath("//*[contains(text(),'Update Quantity')]")).click();
+		driver.findElement(By.cssSelector("button.btn.btn-primary.o_list_button_add")).click();
+	}
+
+	public void login(String x, String y) throws Exception {
 		driver.findElement(By.cssSelector("input[id=login]")).sendKeys(x);
 		driver.findElement(By.cssSelector("input[id=password]")).sendKeys(y);
 		driver.findElement(By.cssSelector("button.btn.btn-primary.btn-block")).click();
 		driver.findElement(By.xpath("//span[@class=\"oe_topbar_name\"]")).isDisplayed();
 
-}
-  @AfterMethod
-  public void afterMethod() {
+	}
 
-	  System.out.println("Browser is closing");
-      driver.quit();	  
-  }	
-  
-  
+	@AfterMethod
+	public void afterMethod() {
+
+		System.out.println("Browser is closing");
+		driver.quit();
+	}
+
 }
--- AspireSeleniumTestng/src/Aspirepac/NewTest.java
+++ AspireSeleniumTestng/src/Aspirepac/NewTest.java
@@ -6,12 +6,6 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterClass;
 
-
-
-
-
-
-
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 
@@ -32,87 +26,77 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 
 public class NewTest {
-	
+
 	public WebDriver driver;
 
-	
-	@Test(priority=0)
-	  public void launch() {
-		  System.setProperty("webdriver.chrome.driver", "E:\\SAspire\\chromedriver.exe");
-	      driver=new ChromeDriver();
-	     driver.get("https://aspireapp.odoo.com/web/login");
-	     WebDriverWait w=new WebDriverWait(driver,30);
-	     w.until(ExpectedConditions.visibilityOfElementLocated(By.name("login")));
-	     String expectedTitle="Odoo";
-	     String actualTitle=driver.getTitle();
-	     Assert.assertEquals(actualTitle, expectedTitle);
-	  }
-	
-	@Test(priority=1)
-	public void validateTitle() throws IOException
-	{
-		//String expectedTitle="Odoo";
-	String pagetitle=driver.getTitle();
-	
-	if(pagetitle.contains("Odoo"))
-	{
-		Assert.assertTrue(true, "title test was passed");
+	@Test(priority = 0)
+	public void launch() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://aspireapp.odoo.com/web/login");
+		WebDriverWait w = new WebDriverWait(driver, 30);
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.name("login")));
+		String expectedTitle = "Odoo";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
 	}
-	else
-	{
-	File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-    File dest=new File("\ss1.png");
-    FileUtils.copyFile(src,dest);
-    Reporter.log("\ss1.png");
-    Assert.assertTrue(false,"title test was failed");
-    
+
+	@Test(priority = 1)
+	public void validateTitle() throws IOException {
+		// String expectedTitle="Odoo";
+		String pagetitle = driver.getTitle();
+
+		if (pagetitle.contains("Odoo")) {
+			Assert.assertTrue(true, "title test was passed");
+		} else {
+			File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File dest = new File("\ss1.png");
+			FileUtils.copyFile(src, dest);
+			Reporter.log("\ss1.png");
+			Assert.assertTrue(false, "title test was failed");
+
+		}
 	}
-	}
-	@Test(dependsOnMethods= {"validateTitle"})
-	@Parameters({"userName","password"})
-	public void login(String x, String y) throws Exception
-	{
+
+	@Test(dependsOnMethods = { "validateTitle" })
+	@Parameters({ "userName", "password" })
+	public void login(String x, String y) throws Exception {
 		driver.findElement(By.cssSelector("input[id=login]")).sendKeys(x);
 		driver.findElement(By.cssSelector("input[id=password]")).sendKeys(y);
 		driver.findElement(By.cssSelector("button.btn.btn-primary.btn-block")).click();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//span[@class=\"oe_topbar_name\"]")).isDisplayed();
-		if(driver.findElement(By.xpath("//span[@class=\"oe_topbar_name\"]")).isDisplayed())
-		{
-			 System.out.println("element is displayed");
-		}
-		else
-		{
-			 System.out.println("element is not displayed");
-		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-	    File dest=new File("\ss2.png");
-	    FileUtils.copyFile(src,dest);
-	    Reporter.log("\ss2.png");
+		if (driver.findElement(By.xpath("//span[@class=\"oe_topbar_name\"]")).isDisplayed()) {
+			System.out.println("element is displayed");
+		} else {
+			System.out.println("element is not displayed");
+			File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File dest = new File("\ss2.png");
+			FileUtils.copyFile(src, dest);
+			Reporter.log("\ss2.png");
 //	    Assert.assertTrue(false,"user test was failed");
-	    
+
 		}
-		}
+	}
 
-	
-  @BeforeMethod
-  public void beforeMethod() {
-	  
-	  System.out.println("Starting the browser session");
-  }
+	@BeforeMethod
+	public void beforeMethod() {
 
-  @AfterMethod 
-  public void afterMethod() {
+		System.out.println("Starting the browser session");
+	}
 
-	  System.out.println("method has been run");
+	@AfterMethod
+	public void afterMethod() {
+
+		System.out.println("method has been run");
 //      driver.quit();	  
-  }
-  
-  @AfterClass
-  public void afterClass() {
+	}
 
-	  System.out.println("Closing the browser session");
-      driver.quit();	  
-  }
+	@AfterClass
+	public void afterClass() {
 
+		System.out.println("Closing the browser session");
+		driver.quit();
+	}
 
 }
